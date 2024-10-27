cc17ff84cf0e125483f4d35734e23b4099c0aff4
Hikmah Nurarifah <105140768+hikmahnurarifah@users.noreply.github.com>
2024-10-27 12:22:24 +0700

    Update datacoffee.js
    coba lagi

--- api/datacoffee.js
+++ api/datacoffee.js
@@ -1,5 +1,14 @@
 import data from '../data/datacoffee.json';
 
 export default function handler(req, res) {
-  res.status(200).json(data);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+  
+    res.status(200).json(data);
 }
